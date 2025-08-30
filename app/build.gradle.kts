fded20ad43ea1b752a247a04f16257a32e2818e3
Art2485 <ooartyguitaroo@gmail.com>
2025-08-30 11:05:11 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,4 @@
 plugins {
-  id("com.android.application")
-  id("org.jetbrains.kotlin.android")
-}
-android {
-  compileSdk = 34
-  defaultConfig {
-    applicationId = "com.recovereasy"
-    minSdk = 24
-    targetSdk = 34
-  }
-}
-dependencies {
-  implementation("androidx.core:core-ktx:1.12.0")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
