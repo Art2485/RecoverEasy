ad82e38e66b19d8353be6b0b10fd5d8e6503e8d4
Art2485 <ooartyguitaroo@gmail.com>
2025-08-31 01:25:34 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
 plugins {
-    // ว่างไว้ ไม่มีปลั๊กอินระดับราก
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
